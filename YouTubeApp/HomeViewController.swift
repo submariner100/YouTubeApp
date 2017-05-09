ec46c53f32338ec43a276d95968e503bdc688cb1
Michael Chapman <submariner100@gmail.com>
2017-05-09 10:33:59 +0100

    first commit

---
+++ YouTubeApp/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  YouTubeApp
+//
+//  Created by Macbook on 09/05/2017.
+//  Copyright © 2017 Chappy-App. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          
+          collectionView?.backgroundColor = UIColor.red
+          
+          
+          
+          
+          
+     }
+
+     
+
+}
+
